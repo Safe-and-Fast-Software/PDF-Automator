e9e914bb97e7f34a056d8e8dafd746583e031987
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-08-11 23:11:17 +0200

    Added some protect pages for which you have to log in first, '/profile', and '/dashboard'.

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,10 +1,32 @@
 import express from 'express';
 import passport from 'passport';
+import constants from '../../constants.js';
 
-const router = express.Router();
+const protectedPathsRouter = express.Router();
 
-router.get('/secure-endpoint', passport.authenticate('oauth2', { session: false }), (req, res) => {
-    res.send(`Hello ${req.user.name}, this is a secure endpoint.`);
+// Middleware to check if user is authenticated
+function isAuthenticated(request, responds, next) {
+    if (request.isAuthenticated()) return next();
+    responds.redirect(constants.app.loginPath);
+}
+
+protectedPathsRouter.get('/dashboard', isAuthenticated, (request, responds) => {
+    responds.send(`Hello ${request.user.name}, welcome to your dashboard!`);
 });
 
-export default router;
+protectedPathsRouter.get('/profile', isAuthenticated, (request, responds) => {
+    const user = request.user;
+    console.log(user);
+    responds.send(
+        `<h1>User Profile</h1>` +
+        "This is all the application knows about you. " +
+        "If you want to change anything you'll have to contact your Oauth provider." +
+        "<br><ul>" +
+        `<li><b>Full Name</b>: ${user.name}</li>` +
+        `<li><b>email</b>: ${user.email}</li>` +
+        `<li><b>username</b>: ${user.username}</li>` +
+        "</ul>"
+    );
+});
+
+export default protectedPathsRouter;
