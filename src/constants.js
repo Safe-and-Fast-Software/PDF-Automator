4b1fc65d5db0090349ee3319c4f540776935c6b6
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-08-11 20:58:23 +0200

    Moved everything that wasn't the constants object from 'src/constants.js' to 'src/environmentVariable.js'.

--- src/constants.js
+++ src/constants.js
@@ -1,39 +1,5 @@
 import dotenv from 'dotenv/config';
-
-/** 
- * A class for throwing exceptions when environment variables are not set.
- */
-export class EnvironmentVariableNotSetError extends Error {
-    constructor(variableName) {
-
-        const message = `Environment variable '${variableName}' is not set.`;
-        super(message);
-
-        this.name = this.constructor.name;
-
-        Error.captureStackTrace(this, this.constructor);
-    }
-}
-
-/**
- * Gets an environment variable with the provided name.
- * 
- * @param { String } name the name of the variable.
- * @param { String | undefined } defaultValue the default value if the environment variable is not set. Throws an error if both are not set.
- * 
- * @returns the environment variable with the provided name
- */
-export function getEnvironmentVariable(name, defaultValue=undefined) {
-
-    const value = process.env[name];
-    const environmentVariableIsNotSet = ( value === null || value === undefined || value === "" );
-
-    if (environmentVariableIsNotSet && defaultValue === undefined) throw new EnvironmentVariableNotSetError(name);
-
-    return value || defaultValue;
-}
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CONSTANTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+import getEnvironmentVariable from './environmentVariable.js';
 
 const constants = {
     app : {
