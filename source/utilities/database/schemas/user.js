364375a7d6febe18c2b357377eb8901aa4a85f44
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-08-25 22:23:56 +0200

    Updated the user and customer schema.

--- source/utilities/database/schemas/user.js
+++ source/utilities/database/schemas/user.js
@@ -3,30 +3,30 @@ import client from '../client.js';
 import Ajv from 'ajv';
 
 const redisSchema = new Schema("customer", {
-    name:        { type: 'string',   caseSensitive: false },
-    phoneNumber: { type: 'string',   caseSensitive: false },
-    email:       { type: 'string',   caseSensitive: false },
-    street1:     { type: 'string',   caseSensitive: false },
-    street2:     { type: 'string',   caseSensitive: false },
-    city:        { type: 'string',   caseSensitive: false },
-    postalCode:  { type: 'string',   caseSensitive: false },
-    country:     { type: 'string',   caseSensitive: false },
+    name:        { type: 'text',   caseSensitive: false },
+    phone:       { type: 'text',   caseSensitive: false },
+    email:       { type: 'text',   caseSensitive: false },
+    street1:     { type: 'text',   caseSensitive: false },
+    street2:     { type: 'text',   caseSensitive: false },
+    city:        { type: 'text',   caseSensitive: false },
+    postalCode:  { type: 'text',   caseSensitive: false },
+    country:     { type: 'text',   caseSensitive: false },
     apiTokenIDs: { type: 'string[]', caseSensitive: false },
 }, { dataStructure: 'JSON' });
 
 /** The JSON schema that every Customer should be in. */
 export const jsonSchema = Object.freeze({
     type: 'object',
-    required: [ "name", "phoneNumber", "email", "street1", "street2", "city", "postalCode", "country" ],
+    required: [ "name", "phone", "email", "street1", "street2", "city", "zip", "country" ],
     additionalProperties: false,
     properties: {
         name:        { type: "string" },
-        phoneNumber: { type: "string" },
+        phone:       { type: "string" },
         email:       { type: "string" },
         street1:     { type: "string" },
         street2:     { type: "string" },
         city:        { type: "string" },
-        postalCode:  { type: "string" },
+        zip:         { type: "string" },
         country:     { type: "string" },
         apiTokenIDs: { type: "array", "items": { "type": "string" } },
     }
