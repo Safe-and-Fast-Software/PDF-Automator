0e4916fd1df451ee7bbe27d6badec09282598e28
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-08-25 15:48:07 +0200

    Followed the pattern I was using in the API route.

--- source/routes/profile/router.js
+++ source/routes/profile/router.js
@@ -5,40 +5,11 @@ import requiresAuthentication from "../../utilities/auth/require-authentication.
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
 import Router from "express";
-const profileRouter = Router();
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Sub-Routes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
-
-// None
+export const router = Router();
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ End Points ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
-
-profileRouter.get('/dashboard', requiresAuthentication, (request, responds) => {
-    responds.send(/*html*/`
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>Dashboard - PDF Automator</title>
-            </head>
-            <body>
-                <h1>Dashboard</h1>
-                <p>
-                    Hello ${request.user.name}, welcome to your dashboard!
-                </p>
-                <h2>Fake Data</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Suscipit rem inventore saepe asperiores similique expedita 
-                    cumque magnam voluptas amet velit neque quis officia commodi 
-                    harum impedit explicabo, rerum provident eius.
-                </p>
-            </body>
-        </html>`
-    );
-}); 
-
-profileRouter.get('/', requiresAuthentication, (request, responds) => {
+router.get('/', requiresAuthentication, (request, responds) => {
     const user = request.user;
     responds.send(/*html*/`
         <!DOCTYPE html>
@@ -63,5 +34,3 @@ profileRouter.get('/', requiresAuthentication, (request, responds) => {
 });
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
-
-export default profileRouter;
