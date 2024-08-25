fa5f63e1e882871bcfd3eaa009c1c339ded9295f
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-08-25 22:22:22 +0200

    Improved search and the HTMX responds.

--- source/routes/api/v1/customer/router.js
+++ source/routes/api/v1/customer/router.js
@@ -25,19 +25,24 @@ router.use("/by-name", byNameRouter);
 router.get("/all", requiresAuthentication, async (request, responds) => {
 
     const isNotAnHtmxRequest = (request.headers["hx-request"] === undefined);
-    const { name, email, phone } = request.query;
+    const { name, phone, email, street1, street2, zip, city, country } = request.query;
 
     try {
 
         let searchQuery = repository.search();
 
         /* Narrowing the search if a query was provided. */ {
-            if (name  && name.trim()  !== "") searchQuery = searchQuery.and('name' ).matches(name);
-            if (email && email.trim() !== "") searchQuery = searchQuery.and('email').matches(email);
-            if (phone && phone.trim() !== "") searchQuery = searchQuery.and('phone').matches(phone);
+            if (name    && name.trim()    !== "") searchQuery = searchQuery.and('name'   ).matches(name);
+            if (phone   && phone.trim()   !== "") searchQuery = searchQuery.and('phone'  ).matches(phone);
+            if (email   && email.trim()   !== "") searchQuery = searchQuery.and('email'  ).matches(email);
+            if (street1 && street1.trim() !== "") searchQuery = searchQuery.and('street1').matches(street1);
+            if (street2 && street2.trim() !== "") searchQuery = searchQuery.and('street2').matches(street2);
+            if (zip     && zip.trim()     !== "") searchQuery = searchQuery.and('zip'    ).matches(zip);
+            if (city    && city.trim()    !== "") searchQuery = searchQuery.and('city'   ).matches(city);
+            if (country && country.trim() !== "") searchQuery = searchQuery.and('country').matches(country);
         }
 
-        const customers = await searchQuery.return.all();
+        const customers = await searchQuery.sortAscending('name').return.all();
               
         /* Returning JSON if it's not an HTMX request. */ {
             if (isNotAnHtmxRequest) return ( responds
@@ -48,24 +53,28 @@ router.get("/all", requiresAuthentication, async (request, responds) => {
         }
 
         return ( /* HTMX */ responds.status(StatusCodes.OK)
-            .send(/*HTML*/`<ul>${customers.map( (customer) => /*HTML*/`
-            <li id="customer-${customer[EntityId]}">
-                <p>
-                    <b>Customer ID</b>: ${customer[EntityId]}<br>
-                    <b>Name</b>: ${customer.name}<br>
-                    <b>Email</b>: ${customer.email}<br>
-                    <b>Phone Number</b>: ${customer.phone}<br>
-                    <b>Street 1</b>: ${customer.street1}<br>
-                    <b>Street 2</b>: ${customer.street2}<br>
-                    <b>zip code</b>: ${customer.zip}<br>
-                    <b>city</b>: ${customer.city}<br>
-                </p>
-                <button hx-delete="/api/v1/customer/by-id/${customer[EntityId]}" hx-trigger="click"
-                    hx-target="#customer-${customer[EntityId]}" hx-swap="outerHTML">
-                    Delete Customer
-                </button>
-            </li>`
-        ).join("")}</ul>`)
+            .send(customers.map( (customer) => /*HTML*/`
+
+                <li id="customer-${customer[EntityId]}">
+                    <p>
+                        <b>Customer ID</b>: ${customer[EntityId]}<br>
+                        <b>Name</b>: ${customer.name}<br>
+                        <b>Email</b>: ${customer.email}<br>
+                        <b>Phone Number</b>: ${customer.phone}<br>
+                        <b>Street 1</b>: ${customer.street1}<br>
+                        <b>Street 2</b>: ${customer.street2}<br>
+                        <b>city</b>: ${customer.city}<br>
+                        <b>zip code</b>: ${customer.zip}<br>
+                        <b>country</b>: ${customer.country}<br>
+                    </p>
+                    <button hx-delete="/api/v1/customer/by-id/${customer[EntityId]}" hx-trigger="click"
+                        hx-target="#customer-${customer[EntityId]}" hx-swap="delete"
+                        hx-on:click="console.log('delete user: ${customer[EntityId]}.')">
+                        Delete Customer
+                    </button>
+                </li>`
+
+            ).join(""))
         );
 
     } catch (error) {
@@ -96,14 +105,14 @@ router.get("/all", requiresAuthentication, async (request, responds) => {
 router.put("/", requiresAuthentication, async (request, responds) => {
 
     const isNotAnHtmxRequest = (request.headers["hx-request"] === undefined);
-    const costumer = request.body;
+    const customer = request.body;
 
-    console.log(`New customer submitted:`, costumer);
+    console.log(`New customer submitted:`, customer);
 
     try {
 
         /* Validating JSON object to follow the custom schema */ {
-            if (! validate(costumer)) return ( responds
+            if (! validate(customer)) return ( responds
                 .status(StatusCodes.BAD_REQUEST)
                 .type('application/json')
                 .send({ 
@@ -113,7 +122,7 @@ router.put("/", requiresAuthentication, async (request, responds) => {
             );
         }
         
-        await repository.save(costumer);
+        const instance = await repository.save(customer);
 
         /* Giving a JSON responds if it's not an HTMX request */ {
             if (isNotAnHtmxRequest) return ( responds
@@ -121,14 +130,32 @@ router.put("/", requiresAuthentication, async (request, responds) => {
                 .send({ 
                     status: StatusCodes.CREATED, 
                     message: `${ReasonPhrases.CREATED}: created customer.`,
-                    costumer
+                    customer
                 })
             );
         }
         
         return ( responds
             .status(StatusCodes.OK)
-            .send(`Successfully created customer!`)
+            .send(/*HTML*/`
+                <li id="customer-${instance[EntityId]}">
+                    <p>
+                        <b>Customer ID</b>: ${instance[EntityId]}<br>
+                        <b>Name</b>: ${instance.name}<br>
+                        <b>Email</b>: ${instance.email}<br>
+                        <b>Phone Number</b>: ${instance.phone}<br>
+                        <b>Street 1</b>: ${instance.street1}<br>
+                        <b>Street 2</b>: ${instance.street2}<br>
+                        <b>city</b>: ${instance.city}<br>
+                        <b>zip code</b>: ${instance.zip}<br>
+                        <b>country</b>: ${instance.country}<br>
+                    </p>
+                    <button hx-delete="/api/v1/customer/by-id/${instance[EntityId]}" hx-trigger="click"
+                        hx-target="#customer-${instance[EntityId]}" hx-swap="delete">
+                        Delete Customer
+                    </button>
+                </li>`
+            )
         ); 
 
     } catch ( error ) {
@@ -141,7 +168,7 @@ router.put("/", requiresAuthentication, async (request, responds) => {
                 .send({ 
                     status: StatusCodes.INTERNAL_SERVER_ERROR, 
                     message: `${ReasonPhrases.INTERNAL_SERVER_ERROR}: could not create new customer.`,
-                    costumer
+                    customer
                 })
             );
         }
