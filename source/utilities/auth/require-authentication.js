6fc7f774dfa5d71beeb6018109388df048b747e7
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-08-25 15:54:45 +0200

    Added a way to temperarily circumvent the lack of API tokens for my tests by adding an ignore auth header.

--- source/utilities/auth/require-authentication.js
+++ source/utilities/auth/require-authentication.js
@@ -5,6 +5,19 @@ import { ReasonPhrases, StatusCodes } from 'http-status-codes';
  */
 export default function requiresAuthentication(request, responds, next) {
     
+    /* Under the right headers and development mode, ignore authorization completely */ {
+        const ignoreAuthorizationHeader = request.headers["x-ignore-authorization-for-this-request"];
+        const requestToIgnoreAuthorization = ignoreAuthorizationHeader === "yes";
+        const environmentIsDevelopment = (process.env.NODE_ENV === "development");
+        if (requestToIgnoreAuthorization && environmentIsDevelopment) {
+            console.warn(
+                `[WARNING] using development headers to IGNORE authorization to: ` +
+                `${request.method}: ${request.originalUrl}`
+            );
+            return next();
+        }
+    }
+
     /* disallowing HTMX unauthorized requests */ {
         if (request.headers['hx-request']) return responds.status(StatusCodes.BAD_REQUEST)
             .send(`${ReasonPhrases.BAD_REQUEST}: Not an HTMX endpoint.`);
@@ -12,5 +25,6 @@ export default function requiresAuthentication(request, responds, next) {
     
     if (request.isAuthenticated()) return next();
 
+    console.log(`Unauthenticated request to ${request.method} at ${request.originalUrl}.`)
     return responds.status(StatusCodes.UNAUTHORIZED).redirect("/auth/login");
 }
