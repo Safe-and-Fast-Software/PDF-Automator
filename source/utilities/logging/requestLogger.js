c898234a265e6e2c74ecc2a28c719d4cad78cf49
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-08-26 14:11:34 +0200

    Added the name of the user to request logs.

--- source/utilities/logging/requestLogger.js
+++ source/utilities/logging/requestLogger.js
@@ -4,10 +4,13 @@
 function requestLogger(request, responds, next) {
 
     const currentTime = new Date().toUTCString();
-
     const ip = (request.headers['x-forwarded-for'] || request.socket.remoteAddress);
+    const name = (request.user&&request.user.name?`${request.user.name}`:"an unknown user");
 
-    console.info(`On ${currentTime} a ${request.method} request was made for ${request.url} by ${ip}.`);
+    console.info(
+        `On ${currentTime} a ${request.method} request was made for ${request.url} by ${name} at ip-address: ${ip}.`
+    );
+    
     if (request.body !== undefined) console.debug('Request body:', request.body);
     // console.debug('Request headers:', request.headers);
 
